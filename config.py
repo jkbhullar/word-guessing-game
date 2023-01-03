591f67a2a9fd0de7ca086ebe3cbad264ef1f2ebe
Jassleen Bhullar <jbhullar@dev-10.com>
2023-01-03 14:04:42 -0800

    basic game works

---
+++ config.py
@@ -0,0 +1,24 @@
+# Dictionary file location, change if needed.
+dictionaryloc = 'dictionary.txt'
+
+# Turn text file location, change if needed.
+turntextloc = 'data/turntext.txt'
+
+# Wheel contents for file location, change if needed.
+wheeltextloc = 'wheeldata.txt'
+
+# End of Round Status status file, change if needed.
+roundstatusloc = 'data/roundstatus.txt'
+
+# number of rounds in a game
+maxrounds = 3
+
+# Vowel cost
+vowelcost = 250
+
+# final prize, you fill this in
+finalprize = 1
+
+# final round, change if needed.
+finalRoundTextLoc = 'data/finalround.txt'
+
